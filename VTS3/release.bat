f5c2b380f65b2d2c6c8662d7048e894678edb261
mikedanner <mikedanner@3407462f-77cb-4f0d-bde7-e815702532b7>
2002-06-14 02:55:35 +0000

    Batch file to create the distribution files.

---
+++ VTS3/release.bat
@@ -0,0 +1,21 @@
+@echo off
+REM This batch file constructs the distribution files for VTS 3.x
+REM It requires the command line version of pkzip 2.5 for Win32.
+REM
+REM The user must specify the root name of the release as the first
+REM command line argument, i.e. "vts-1.3.6"
+REM
+REM This should be invoked from the VTS3 root.
+REM
+REM --- First make the executable distribution ---
+del %1-win32.zip
+pkzip25 -add %1-win32.zip release\VTS.exe
+cd docs
+pkzip25 -add -path ..\%1-win32.zip *.html	images\*.gif
+cd ..
+pkzip25 -add -path %1-win32.zip scripts\*.vts
+REM --- Then make the source distribution ---
+del %1-source.zip
+pkzip25 -add -dir %1-source.zip *.cpp *.hpp *.c *.h
+pkzip25 -add -dir %1-source.zip *.bmp *.cur *.ico *.rc *.rc2 *.dsp *.dsw *.lib *.vts
+
