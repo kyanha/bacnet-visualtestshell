004e3fa1f14184b55af1deb43f5eb9ccb25531a5
madanner <madanner@3407462f-77cb-4f0d-bde7-e815702532b7>
2002-12-11 04:00:35 +0000

    Moved the queue handler's code to ScriptExecutor and made the queue header only contain the class definition.  This was necessary to include the queue class somewhere else (for processing off the queue MAKE and CHECK dialog commands).

--- trunk/VTS3/VTSQueue.h
+++ trunk/VTS3/VTSQueue.h
@@ -44,99 +44,10 @@ template<class T>
 
 			T* Read( void );					// nil if empty
 			void Write( T* t );					// add a pointer to an element
+			void Fire( T* t );					// write to the queue then fire to waiting thread
 		};
 
-
-
-//
-//	VTSQueue<T>::VTSQueue
-//
-
-template<class T>
-	VTSQueue<T>::VTSQueue()
-		: qFirst(0), qLast(0)
-	{
-	}
-
-//
-//	VTSQueue<T>::~VTSQueue
-//
-
-template<class T>
-	VTSQueue<T>::~VTSQueue()
-	{
-		if (qFirst)
-			TRACE0( "Warning: queue not empty\n" );
-	}
-
-//
-//	VTSQueue<T>::Read
-//
-//	This function returns a pointer to the first (oldest) element that was
-//	added to the queue.  If the queue is empty it returns nil.
-//
-
-template<class T>
-	T* VTSQueue<T>::Read( void )
-	{
-		// lock the queue
-		qCS.Lock();
-
-		VTSQueueElem	*cur = qFirst
-		;
-		T				*rslt = 0
-		;
-
-		// if the queue not is empty, extract the first element
-		if (cur) {
-			// set result
-			rslt = cur->qElem;
-
-			// remove from list
-			qFirst = cur->qNext;
-			if (!qFirst)
-				qLast = 0;
-
-			// delete wrapper element
-			delete cur;
-		}
-
-		// unlock the queue
-		qCS.Unlock();
-
-		// fini
-		return rslt;
-	}
-
-//
-//	VTSQueue<T>::Write
-//
-//	This function adds a pointer to an element to the end of the queue.
-//
-
-template<class T>
-	void VTSQueue<T>::Write( T* tp )
-	{
-		// lock the queue
-		qCS.Lock();
-
-		VTSQueueElem	*cur = new VTSQueueElem()
-		;
-
-		cur->qElem = tp;
-		cur->qNext = 0;
-
-		if (qFirst)
-			qLast->qNext = cur;
-		else
-			qFirst = cur;
-
-		qLast = cur;
-
-		// unlock the queue
-		qCS.Unlock();
-	}
-
-
+class ScriptExecMsg;
+typedef VTSQueue<ScriptExecMsg> ScriptExecMsgQueue;
 
 #endif // !defined(AFX_VTSQUEUE_H__AC6F4361_C02A_11D4_BEFB_00A0C95A9812__INCLUDED_)
