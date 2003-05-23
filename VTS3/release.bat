e4df4536c1b6854055b5d0b4a9c99557d3d6a440
mikedanner <mikedanner@3407462f-77cb-4f0d-bde7-e815702532b7>
2003-05-23 03:03:00 +0000

    Removed BACMACNT, added WinPcap

--- VTS3/release.bat
+++ VTS3/release.bat
@@ -9,14 +9,15 @@ REM This should be invoked from the VTS3 root.
 REM
 REM --- First make the executable distribution ---
 del %1-win32.zip
-pkzip25 -add %1-win32.zip release\VTS.exe release\ptp.dll
+pkzip25 -add %1-win32.zip release\VTS.exe release\ptp.dll winpcap\*.exe
 cd docs
 pkzip25 -add -path ..\%1-win32.zip *.html images\*.gif *.doc
 cd ..
-pkzip25 -add -path %1-win32.zip scripts\*.vts bacmacNT\*.*
+pkzip25 -add -path %1-win32.zip scripts\*.vts
 REM --- Then make the source distribution ---
 del %1-source.zip
 pkzip25 -add -dir %1-source.zip *.cpp *.hpp *.c *.h *.inl 
 pkzip25 -add -dir %1-source.zip *.bmp *.cur *.ico *.rc *.rc2 *.dsp *.dsw *.lib *.vts *.def
-pkzip25 -add -path %1-source.zip bacmacNT\*.*
+REM pkzip25 -add -path %1-source.zip bacmacNT\*.*
+pkzip25 -add -path %1-source.zip winpcap\*.exe
 
