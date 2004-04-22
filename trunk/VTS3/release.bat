4bb9af21bf8a6e73704ec9abb0e08a1b4979ce4f
mikedanner <mikedanner@3407462f-77cb-4f0d-bde7-e815702532b7>
2004-04-22 20:05:08 +0000

    Added commands to include NBLink files

--- trunk/VTS3/release.bat
+++ trunk/VTS3/release.bat
@@ -3,13 +3,16 @@ REM This batch file constructs the distribution files for VTS 3.x
 REM It requires the command line version of pkzip 2.5 for Win32.
 REM
 REM The user must specify the root name of the release as the first
-REM command line argument, i.e. "vts-1.3.6"
+REM command line argument, i.e. "vts-3.4.5"
 REM
 REM This should be invoked from the VTS3 root.
+REM
+REM "-dir" is the same as combining "-path" and "-recurse"
 REM
 REM --- First make the executable distribution ---
 del %1-win32.zip
 pkzip25 -add %1-win32.zip release\VTS.exe release\ptp.dll winpcap\*.exe
+pkzip25 -add %1-win32.zip NBLink\release\nb_link_settings.dll 
 cd docs
 pkzip25 -add -path ..\%1-win32.zip *.html images\*.png *.doc
 cd ..
@@ -20,4 +23,5 @@ pkzip25 -add -dir %1-source.zip *.cpp *.hpp *.c *.h *.inl
 pkzip25 -add -dir %1-source.zip *.bmp *.cur *.ico *.rc *.rc2 *.dsp *.dsw *.lib *.vts *.def
 REM pkzip25 -add -path %1-source.zip bacmacNT\*.*
 pkzip25 -add -path %1-source.zip winpcap\*.exe
+pkzip25 -add -dir %1-source.zip nblink\*.dll 
 
