5e0baae164009f6ebd9326209d6d7d671fc80e66
madanner <madanner@3407462f-77cb-4f0d-bde7-e815702532b7>
2004-09-06 16:13:12 +0000

    Added destructors.. these guys weren't freeing properly.

--- trunk/VTS3/WPRPList.h
+++ trunk/VTS3/WPRPList.h
@@ -38,12 +38,26 @@ class WPMRPMList : public CList<WPMRPMElemPtr, WPMRPMElemPtr>
 public:
 	CString m_ObjIDStr;
 	BACnetObjectIdentifier	m_ObjID;
+
+	//madanner 9/04, fixed massive memory leaks :(  by adding destructor
+	WPMRPMList(){};
+	~WPMRPMList();
+
+	void DeleteAllObjects(void);
+	void KillAll(void);
 };
 
 typedef WPMRPMList *WPMRPMListPtr;
 
 class WPMRPMListList : public CList<WPMRPMListPtr, WPMRPMListPtr>
 {
+public:
+	//madanner 9/04, fixed massive memory leaks :(
+	WPMRPMListList() {};
+	~WPMRPMListList();
+
+	void DeleteAllObjects(void);
+	void KillAll(void);
 };
 
 #endif // !defined(AFX_WPRPLIST_H__AF524C1A_9CD4_4C9B_9CD5_C66546E641EE__INCLUDED_)
