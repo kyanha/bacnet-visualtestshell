8d4828b696e995919d367923f1c67e38480be910
johnhartman <johnhartman@3407462f-77cb-4f0d-bde7-e815702532b7>
2010-06-01 21:02:12 +0000

    Make template selection dialog resizeable

--- VTS3/SelectTemplateDlg.cpp
+++ VTS3/SelectTemplateDlg.cpp
@@ -35,6 +35,7 @@ void SelectTemplateDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(SelectTemplateDlg, CDialog)
 	//{{AFX_MSG_MAP(SelectTemplateDlg)
 	ON_LBN_DBLCLK(IDC_LIST1, OnOK)
+	ON_WM_SIZE()
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -63,4 +64,47 @@ void SelectTemplateDlg::OnOK()
 	}
 	
 	CDialog::OnOK();
+}
+
+void SelectTemplateDlg::OnSize(UINT nType, int cx, int cy) 
+{
+	CDialog::OnSize(nType, cx, cy);
+	
+	CWnd *pOK = GetDlgItem( IDOK );
+	CWnd *pCancel = GetDlgItem( IDCANCEL );
+	if (m_hWnd && m_templateList.m_hWnd && pOK && pCancel)
+	{
+		// This code would be much simpler if GetClientRect returned the 
+		// client POSITION relative to its containing window as well as the size...
+
+		// Get size of the dialog
+		CRect rect;
+		GetClientRect(&rect);
+
+		// Get size and position of the list
+		CRect rectList;
+		m_templateList.GetClientRect( &rectList );
+
+		// Get sizes of the ok and cancel buttons
+		CRect rectOK, rectCancel;
+		pOK->GetClientRect( &rectOK );
+		pCancel->GetClientRect( &rectCancel );
+
+#define DA_BORDER 8
+
+		// Resize list: entire dialog, except for the buttons near the bottom
+		m_templateList.MoveWindow( DA_BORDER, DA_BORDER, 
+								   rect.Width() - 2*DA_BORDER, 
+								   rect.Height() - rectOK.Height() - 3*DA_BORDER );
+
+		// Move the ok and cancel buttons:
+		// Just above the bottom, and centered in the dialog
+		int okPos = (rect.Width() - rectOK.Width() - rectCancel.Width() - 2*DA_BORDER)/2;
+		pOK->MoveWindow( okPos,
+						 rect.Height() - rectOK.Height() - DA_BORDER, 
+						 rectOK.Width(), rectOK.Height() );
+		pCancel->MoveWindow( okPos + rectOK.Width() + 2*DA_BORDER,
+							 rect.Height() - rectCancel.Height() - DA_BORDER, 
+							 rectCancel.Width(), rectCancel.Height() );
+	}
 }
